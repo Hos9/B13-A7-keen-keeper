737e7754d8a46db323185aaaeedb2b284672b3f2
Hos9 <shahin9315@gmail.com>
2026-04-14 14:49:31 +0600

    Assignment 7: B13-A7-keen-keeper Initiated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-lime-400/30 rounded-2xl my-10 min-h-[30vh] font-bold flex justify-center items-center text-3xl container mx-auto">
+        Vite + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
